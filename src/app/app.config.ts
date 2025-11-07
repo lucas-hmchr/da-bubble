7b76693bdaaee35141344c6875df5b3d00f38542
lucas <lucas.hamacher@ma-versand.de>
2025-11-07 10:42:56 +0100

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,26 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "da-bubble-bc7ad",
+      appId: "1:558692292679:web:87a82ca7d82e5672717962",
+      storageBucket: "da-bubble-bc7ad.firebasestorage.app",
+      apiKey: "AIzaSyDDWhyr7gKX_0CraOyxFxwgdIYhLRk-fBM",
+      authDomain: "da-bubble-bc7ad.firebaseapp.com",
+      messagingSenderId: "558692292679"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase())
   ]
 };
