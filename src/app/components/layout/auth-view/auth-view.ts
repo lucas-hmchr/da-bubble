eaabf284f033cd4813afba48bcaea16e12b21a36
lucas <lucashamacher01@gmail.com>
2025-11-11 15:39:53 +0100

    add first layout and styling for login/auth forms

--- src/app/components/layout/auth-view/auth-view.ts
+++ src/app/components/layout/auth-view/auth-view.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Logo } from "../../shared/logo/logo";
+import { LoginForm } from "./login-form/login-form";
 
 @Component({
   selector: 'app-auth-view',
-  imports: [],
+  imports: [Logo, LoginForm],
   templateUrl: './auth-view.html',
   styleUrl: './auth-view.scss',
 })
