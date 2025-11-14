e9864fdf8c416223f05ca8db4b001a6492ceaa27
Steven <bracke.s85@gmail.com>
2025-11-14 13:09:02 +0100

    creating message-input.component

--- src/app/components/layout/auth-view/password-forgot/password-forgot.ts
+++ src/app/components/layout/auth-view/password-forgot/password-forgot.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from "@angular/router";
+import { MessageInput } from '../../../shared/message-input/message-input';
 
 @Component({
   selector: 'app-password-forgot',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, MessageInput],
   templateUrl: './password-forgot.html',
   styleUrl: './password-forgot.scss',
 })
