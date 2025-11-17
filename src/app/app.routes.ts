d25dfdfc997a0e31fdf9fda30574af800fe895e5
lucas <lucashamacher01@gmail.com>
2025-11-17 12:02:32 +0100

    remove import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { AppShell } from './components/layout/app-shell/app-shell';
-import { AuthView } from './components/layout/auth-view/auth-view';
 
 export const routes: Routes = [
     { path: '', component: AppShell },
