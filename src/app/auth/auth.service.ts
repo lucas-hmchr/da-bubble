1fea4d8087634f348c98fad6fb736b018010b6a3
lucas <lucashamacher01@gmail.com>
2025-11-18 13:01:26 +0100

    adjust auth functions and add login with google

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,9 +7,13 @@ import {
     updateProfile,
     sendPasswordResetEmail,
     User,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signInWithRedirect,
+    UserCredential,
 } from '@angular/fire/auth';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { Firestore, doc, serverTimestamp, setDoc } from '@angular/fire/firestore';
+import { Firestore, doc, getDoc, serverTimestamp, setDoc } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 
 @Injectable({
@@ -21,35 +25,63 @@ export class AuthService {
     private user$ = user(this.auth);
     private firestore = inject(Firestore);
     private router = inject(Router)
+    googleProvider = new GoogleAuthProvider();
 
-    readonly userSignal = toSignal<User | null>(this.user$, { initialValue: null });
-    readonly isLoggedIn = computed(() => !!this.userSignal());
-    readonly uid = computed(() => this.userSignal()?.uid ?? null);
+    readonly activeUser = toSignal<User | null>(this.user$, { initialValue: null });
+    readonly isLoggedIn = computed(() => !!this.activeUser());
+    readonly uid = computed(() => this.activeUser()?.uid ?? null);
 
-    async register(email: string, password: string, displayName: string, avatarName: string): Promise<User> {
+    async register(email: string, password: string, displayName: string, avatarName: string): Promise<void> {
         const cred = await createUserWithEmailAndPassword(this.auth, email, password);
         await updateProfile(cred.user, {
             displayName,
             photoURL: `/images/avatars/${avatarName}.svg`,
         });
-        const userRef = doc(this.firestore, 'users', cred.user.uid)
-        await setDoc(userRef, {
-            uid: cred.user.uid,
-            email,
-            displayName,
-            createdAt: serverTimestamp(),
-        });
+        await this.createUserDocForNewUser(cred.user, { avatarName });
         this.router.navigate(['/auth'])
-        return cred.user;
     }
 
-    async login(email: string, password: string): Promise<User> {
-        const cred = await signInWithEmailAndPassword(this.auth, email, password);
-        return cred.user;
+    getUserRef(uid: string = this.activeUser()!.uid) {
+        const userRef = doc(this.firestore, 'users', uid);
+        return userRef;
+    }
+
+    private async createUserDocForNewUser(user: User, opts?: { avatarName?: string }) {
+        const { avatarName } = opts ?? {};
+        const ref = this.getUserRef(user.uid);
+
+        await setDoc(ref, {
+            uid: user.uid,
+            email: user.email,
+            displayName: user.displayName,
+            avatarName: avatarName ?? null,
+            isOnline: false,
+            createdAt: serverTimestamp(),
+        });
+    }
+
+    async login(email: string, password: string): Promise<void> {
+        await signInWithEmailAndPassword(this.auth, email, password);
+    }
+
+    async signInWithGoogle(): Promise<void> {
+        try {
+            const cred = await signInWithPopup(this.auth, this.googleProvider);
+            const user = cred.user;
+            const snap = await getDoc(this.getUserRef(user.uid));
+            if (!snap.exists()) {
+                await this.createUserDocForNewUser(user);
+            }
+            this.router.navigate(['/']);
+        } catch (error: any) {
+            console.error('Google Login Fehler:', error);
+            throw error;
+        }
     }
 
     async logout(): Promise<void> {
         await signOut(this.auth);
+        this.router.navigate(['/auth']);
     }
 
     async resetPassword(email: string): Promise<void> {
