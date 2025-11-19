b449c8ce7dc110d3dbaa51a8da263c4c83c7643f
lucas <lucashamacher01@gmail.com>
2025-11-19 13:04:26 +0100

    set env variables for auth guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { AppShell } from './components/layout/app-shell/app-shell';
 import { redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard';
+import { environment } from '../environments/environment';
 
 const redirectUnauthorizedToAuth = () => redirectUnauthorizedTo(['auth']);
-const redirectLoggedInToApp  = () => redirectLoggedInTo(['']);
+const redirectLoggedInToApp = () => redirectLoggedInTo(['']);
+
+
+const guardsEnabled = environment.enableRouteGuards;
+const guardIfEnabled = (pipe: any) => (guardsEnabled ? canActivate(pipe) : {});
 
 export const routes: Routes = [
-    { path: '', component: AppShell, ...canActivate(redirectUnauthorizedToAuth), },
-    { path: 'auth',  loadChildren: () =>
-      import('./auth/auth.routes').then(m => m.AUTH_ROUTES), ...canActivate(redirectLoggedInToApp), },
+  { path: '', component: AppShell, ...guardIfEnabled(redirectUnauthorizedToAuth)},
+  {
+    path: 'auth', loadChildren: () =>
+      import('./auth/auth.routes').then(m => m.AUTH_ROUTES), ...guardIfEnabled(redirectLoggedInToApp)},
 ];
