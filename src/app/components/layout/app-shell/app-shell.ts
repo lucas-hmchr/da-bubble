cc0643294bdaa21224f43890caeb36b1a8709818
Steven <bracke.s85@gmail.com>
2025-11-24 14:21:10 +0100

    from belal

--- src/app/components/layout/app-shell/app-shell.ts
+++ src/app/components/layout/app-shell/app-shell.ts
@@ -2,6 +2,8 @@ import { Component, effect } from '@angular/core';
 import { Topbar } from "../topbar/topbar";
 import { View } from '../view/view';
 import { AuthService } from '../../../auth/auth.service';
+import { FirestoreService } from '../../../services/firestore';
+import { Avatar } from '../../../models/user.model';
 
 @Component({
   selector: 'app-app-shell',
@@ -10,9 +12,21 @@ import { AuthService } from '../../../auth/auth.service';
   styleUrl: './app-shell.scss',
 })
 export class AppShell {
-  constructor(private authService: AuthService) {
+  users: Avatar[] = [];
+  usersJson = '';
+
+  constructor(
+    private authService: AuthService,
+    private firestoreService: FirestoreService,
+  ) {
     effect(() => {
       console.log('activeUser in AppShell:', this.authService.activeUser());
+    });
+
+    this.firestoreService.getCollection<Avatar>('users').subscribe((users) => {
+      this.users = users;
+      this.usersJson = JSON.stringify(users);
+      console.log('users in AppShell:', this.users);
     });
   }
 }
