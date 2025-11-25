94aaea755f11a5679ceaae534d0f529e230d94d0
Bobi-DA <masterjb81@gmail.com>
2025-11-25 23:28:33 +0100

    Update workspace-sidebar layout & styling

--- src/app/components/layout/workspace-sidebar/workspace-sidebar.ts
+++ src/app/components/layout/workspace-sidebar/workspace-sidebar.ts
@@ -1,8 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatIconModule} from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-workspace-sidebar',
