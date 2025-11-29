dd189fede117325a7bcfb803ea730f800f29e24d
Setsuna <you@example.com>
2025-11-29 21:40:18 +0100

    search bar + profil2

--- src/app/components/layout/topbar/topbar.ts
+++ src/app/components/layout/topbar/topbar.ts
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './topbar.scss',
 })
 export class Topbar {
+  isDropdownMenuOpen = false;
+  isProfilModalOpen = false;
 
+  toggleDropdownMenu(event: Event) {
+    event.stopPropagation();
+    this.isDropdownMenuOpen = !this.isDropdownMenuOpen;
+  }
+
+  openProfilModal() {
+    this.isProfilModalOpen = true;
+    this.isDropdownMenuOpen = false;
+  }
+
+  closeProfilModal() {
+    this.isProfilModalOpen = false;
+  }
+
+  ngOnInit() {
+    document.addEventListener('click', () => {
+      this.isDropdownMenuOpen = false;
+    });
+  }
 }
