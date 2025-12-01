a6d3e511b06e92312a140ccc87c18f255debaa45
Bobi-DA <masterjb81@gmail.com>
2025-12-01 08:41:26 +0100

    create component add-channel-dialog

---
+++ src/app/components/add-channel-dialog/add-channel-dialog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-channel-dialog',
+  imports: [],
+  templateUrl: './add-channel-dialog.html',
+  styleUrl: './add-channel-dialog.scss',
+})
+export class AddChannelDialog {
+
+}
