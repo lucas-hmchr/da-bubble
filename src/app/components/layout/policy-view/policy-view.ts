b0f65689517d862446756ab4eae1d0b3d549b182
Setsuna <you@example.com>
2025-12-10 12:38:49 +0100

    add imprint and policy

---
+++ src/app/components/layout/policy-view/policy-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-policy-view',
+  imports: [],
+  templateUrl: './policy-view.html',
+  styleUrl: './policy-view.scss',
+})
+export class PolicyView {
+
+}
