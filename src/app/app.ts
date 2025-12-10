a3e3ee365880440c0de5267eb33b45867e4f3871
lucas <lucashamacher01@gmail.com>
2025-12-10 08:52:02 +0100

    remove unused imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WorkspaceSidebar } from "./components/layout/workspace-sidebar/workspace-sidebar";
 import { AuthService } from './auth/auth.service';
 import { Toast } from './components/shared/toast/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WorkspaceSidebar, Toast],
+  imports: [RouterOutlet, Toast],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
