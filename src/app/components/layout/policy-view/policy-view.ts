78b58d4642f39fda51b2c9bf4bc2a98d772c76ba
Setsuna <you@example.com>
2025-12-10 17:04:44 +0100

    add form from login

--- src/app/components/layout/policy-view/policy-view.ts
+++ src/app/components/layout/policy-view/policy-view.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-policy-view',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './policy-view.html',
   styleUrl: './policy-view.scss',
 })
 export class PolicyView {
+  private router = inject(Router);
+  private authService = inject(AuthService);
 
+  email = '';
+  password = '';
+
+  showEmailLoginError: boolean = false;
+
+  async submitLogin() {
+    const loginSuccess = await this.authService.login(this.email, this.password);
+    this.showEmailLoginError = loginSuccess ? false : this.resetLoginForm();
+  }
+
+  triggerGoogleLogin() {
+    this.authService.signInWithGoogle();
+  }
+
+  continueAsGuest() {
+    this.authService.loginAsGuest();
+  }
+
+  resetLoginForm() {
+    this.password = '';
+    this.email = '';
+    return true;
+  }
 }
