448e33cd8437d6140aef07f6eab11e77aa29c2f7
Bobi-DA <masterjb81@gmail.com>
2025-12-21 23:09:09 +0100

    app-shell resp, not final

--- src/app/components/layout/app-shell/app-shell.ts
+++ src/app/components/layout/app-shell/app-shell.ts
@@ -1,21 +1,29 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, effect, HostListener, inject, signal } from '@angular/core';
 import { Topbar } from '../topbar/topbar';
 import { View } from '../view/view';
 import { WorkspaceSidebar } from '../workspace-sidebar/workspace-sidebar';
 import { ThreadMenu } from '../thread-menu/thread-menu';
 import { AuthService } from '../../../auth/auth.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+export type MobileView = 'sidebar' | 'chat' | 'thread';
 
 @Component({
   selector: 'app-app-shell',
   standalone: true,
-  imports: [Topbar, View, WorkspaceSidebar, ThreadMenu],
+  imports: [Topbar, View, WorkspaceSidebar, ThreadMenu, MatIconModule, MatButtonModule],
   templateUrl: './app-shell.html',
   styleUrl: './app-shell.scss',
 })
+
+
 export class AppShell {
   currentUserUid: string | null = null;
-
+  isNewMessageMode = signal(false);
+  activeMobileView = signal<MobileView>('sidebar');
   private authService = inject(AuthService);
+  isMobile = signal(window.innerWidth < 1024);
 
   constructor() {
     effect(() => {
@@ -23,5 +31,25 @@ export class AppShell {
       console.log('activeUser in AppShell:', active);
       this.currentUserUid = active?.uid ?? null;
     });
+    window.addEventListener('resize', () => {
+      this.isMobile.set(window.innerWidth < 1024);
+    });
   }
+
+  openNewMessage() {
+    // Mobile: erst auf Chat wechseln
+    if (this.isMobile()) {
+      this.activeMobileView.set('chat');
+    }
+
+    // hier sagen wir später dem View:
+    // "zeige neue Nachricht"
+    this.isNewMessageMode.set(true);
+
+  }
+
+  closeNewMessage() {
+    this.isNewMessageMode.set(false);
+  }
+
 }
